5b41e67a523c3f283554af5c3994f90add1a59af
Luma Montes <lumagoesmontes@gmail.com>
2020-09-06 04:53:41 -0300

    finalizado estrutura html

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
 import './style.css'
+import { Link } from 'react-router-dom'
 
 function Home () {
     return (
-        <p>oi</p>
+       <div id="home-page">
+           <div id="home-page-content" className="container">
+            <header>
+                <img src="" alt=""/>
+                <nav>
+                    <Link to="#" className="button">Entrar</Link>
+                    <Link to="#" className="">Cadastre-se</Link>
+                 </nav>
+            </header>
+        <div id="title-container">
+            <h1>Você precisa fazer o teste para a Covid-19? </h1>
+            <p>Faça o teste rápido para a covid-19 gratuitamente, realizando o seu cadastro e agendamento pela plataforma :)</p>
+            <Link to="#" className="button2">Começar</Link>
+        </div>
+
+           </div>
+       </div>
     )
 }
 

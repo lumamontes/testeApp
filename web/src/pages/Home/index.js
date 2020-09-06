7802010c79c08872f1df1f6038dc882011552cb3
Luma Montes <lumagoesmontes@gmail.com>
2020-09-06 18:41:03 -0300

    finalizado estrutura da home

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Logo from '../../assets/images/hospital.svg'
+import Doutores from '../../assets/images/doutores.jpg'
 import './style.css'
 import { Link } from 'react-router-dom'
 
@@ -8,19 +9,29 @@ function Home () {
        <div id="home-page">
            <div id="home-page-content" className="container">
             <header>
-                <img src="" alt=""/>
+                <img src={Logo} alt="logo"/>
+
                 <nav>
                     <Link to="#" className="button">Entrar</Link>
                     <Link to="#" className="">Cadastre-se</Link>
                  </nav>
             </header>
-        <div id="title-container">
+
+            <div id="title-container">
+            <div className="text">
             <h1>Você precisa fazer o teste para a Covid-19? </h1>
             <p>Faça o teste rápido para a covid-19 gratuitamente, realizando o seu cadastro e agendamento pela plataforma :)</p>
             <Link to="#" className="button2">Começar</Link>
-        </div>
+            </div>
+
+            <div id="home-page-img"> 
+                <img src={Doutores} alt=""/>
+            </div>
+          </div>
+
 
            </div>
+
        </div>
     )
 }

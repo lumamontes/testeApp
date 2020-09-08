977c22221b0190b2319621cf60f2cfc661f2b7a7
Luma Montes <lumagoesmontes@gmail.com>
2020-09-07 23:34:05 -0300

    iniciado pagina de cadastro

---
+++ web/src/pages/Form/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
+
+function Form () {
+    return (
+        <div id="home-page">
+            <div id="home-page-content" className="container">
+
+              <form action="">
+                <h1>Cadastro</h1>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit..</h3>
+
+                <label htmlFor="">
+                    <input type="text"/>
+                </label>
+              
+              </form>
+            </div>
+
+            <div className="img-container">
+                <img src="" alt=""/>
+            </div>
+
+         </div>
+
+        )
+}
+
+export default Form

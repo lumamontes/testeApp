020c3067c2bdaa8ab3c43ba0f22e74c9aa411d12
Luma Montes <lumagoesmontes@gmail.com>
2020-09-07 23:34:28 -0300

    adicionado rota da pagina de cadastro

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import RegisterForm from './pages/Form'
 
 function Routes () {
     return (
         <BrowserRouter>
         <Route path="/" exact component={Home}/>
+        <Route path="/cadastro" exact component={RegisterForm}/>
         </BrowserRouter>
     )
 }

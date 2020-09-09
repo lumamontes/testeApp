4f166ecc752c97fe2e09f3bcd5eacc19f027286f
Luma Montes <lumagoesmontes@gmail.com>
2020-09-08 23:42:05 -0300

    finalizado estrutura da pagina de cadastro

--- web/src/pages/Form/index.js
+++ web/src/pages/Form/index.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+import { FaLongArrowAltLeft} from 'react-icons/fa';
 import './style.css'
 
 function Form () {
     return (
-        <div id="home-page">
-            <div id="home-page-content" className="container">
-
+        <div id="pacient-form-page">
+            <div id="pacient-form-content" className="container">
+            <FaLongArrowAltLeft/>
               <form action="">
                 <h1>Cadastro</h1>
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit..</h3>
 
-                <label htmlFor="">
-                    <input type="text"/>
+                <label htmlFor="name">
+                    <input type="text" placeholder="nome"/>
                 </label>
-              
-              </form>
+
+                <label htmlFor="lastname">
+                    <input type="text" placeholder="sobrenome"/>
+                </label>
+
+                <label htmlFor="cpf">
+                    <input type="text" placeholder="CPF"/>
+                </label>
+
+                <label htmlFor="password">
+                    <input type="password" placeholder="senha"/>
+                </label>
+
+
+                  <input type="checkbox" name="termos" id=""/>
+                  Concordo com os termos de Privacidade
+                 <Link to="#" className="button"> Cadastrar </Link>
+                </form>
             </div>
 
             <div className="img-container">
